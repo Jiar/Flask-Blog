9020baf31ef9b9ecc8dd0bc5ea53aee7cdaf8dff
Jiar <jiar.world@gmail.com>
2017-01-11 16:25:19 +0800

    完成：9-12

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Flask-Blog
 
-学习自：[JmilkFan - 用 Flask 来写个轻博客](http://blog.csdn.net/Jmilk/article/category/6518106/1)
+学习自：
+[JmilkFan - 用 Flask 来写个轻博客](http://blog.csdn.net/Jmilk/article/category/6518106/1)
+[Github地址：](https://github.com/JmilkFan/JmilkFan-s-Blog)
--- main.py
+++ main.py
@@ -8,16 +8,12 @@ from flask import Flask
 from config import DevConfig
 
 app = Flask(__name__)
+# Import the views module
+views = __import__('views')
 
 # Get the config from object of DecConfig
 # 使用 onfig.from_object() 而不使用 app.config['DEBUG'] 是因为这样可以加载 class DevConfig 的配置变量集合，而不需要一项一项的添加和修改。
 app.config.from_object(DevConfig)
-
-# 指定 URL='/' 的路由规则
-# 当访问 HTTP://server_ip/ GET(Default) 时，call home()
-@app.route('/')
-def home():
-    return '<h1>Hello World!</h1>'
 
 if __name__ == '__main__':
     # Entry the application
